9e4c8149d3020b7846b26e764a84b20ee8ca711b
SabitDeepto <deepto69@gmail.com>
2019-01-02 13:45:47 +0600

    pos

--- pos/views.py
+++ pos/views.py
@@ -5,42 +5,16 @@ from pos.forms import WordForm, TestForm
 from pos.models import Word
 
 
-def home(request):
+def test(request):
     form = WordForm(request.POST)
 
     if form.is_valid():
         list_of_words = form.cleaned_data.get('word').split(' ')
         for x in list_of_words:
             Word.objects.create(word=x)
-        list_of_pos = [form.cleaned_data.get('pos')]
+        # list_of_pos = [form.cleaned_data.get('pos')]
 
-        words = zip(list_of_words, list_of_pos)
-        result = dict(words)
+        return render(request, 'index.html', {'query': list_of_words, 'form': form})
 
-        return render(request, 'design.html', {'query': list_of_words, 'form': form})
+    return render(request, 'index.html', {'form': form})
 
-    return render(request, 'design.html', {'form': form})
-
-# def some_view(request):
-#     if request.method == 'POST':
-#         text = request.POST.get('field_name')
-#         if text:
-#             for line in text.split('\n'):
-#                 SomeModel.objects.create(field_name=line)
-
-
-#
-# def home(request):
-#     form = WordForm(request.POST)
-#
-#     if form.is_valid():
-#         list_of_words = form.cleaned_data.get('word').split(' ')
-#         list_of_pos = [form.cleaned_data.get('pos')]
-#
-#         words = zip(list_of_words, list_of_pos)
-#         result = dict(words)
-#
-#         print(len(list_of_words))
-#         return render(request, 'design.html', {'query': list_of_words, 'form': form})
-#
-#     return render(request, 'design.html', {'form': form})
